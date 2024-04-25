d533fa414c88d8810418dfc235f3410105bc3478
RodrigoERodriguez <rodrigoerodriguez48@gmail.com>
2024-04-25 19:32:13 -0300

    -

--- routes/clientes.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from ""
-
-const router = Router()
-
-router.get('/Saludo', (req, res) => {
-    res.status(200).json({Saludo: "Saludo del Cliente"})
-})
-
-export default router
--- routes/mesa.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from ""
-
-const router = Router()
-
-router.get('/Saludo', (req, res) => {
-    res.status(200).json({Saludo: "Saludo del Cliente"})
-})
